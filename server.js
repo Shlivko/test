8b074b5c059a58d79d971aa82db55c9ef0fe8e38
Shlivko <86900770+Shlivko@users.noreply.github.com>
2025-11-29 08:46:08 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 const pdfPath = path.join(__dirname, 'public', 'check', 'reference-1242080012023055.pdf');
 
@@ -26,4 +25,5 @@ app.use((req, res) => {
     message: 'Access denied'
   });
 });
+
 
