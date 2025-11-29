0398fa97e683b85d18535577c9c8e9471c54c534
Shlivko <86900770+Shlivko@users.noreply.github.com>
2025-11-29 08:53:29 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
-app.use(express.static(path.join(__dirname, 'public')));
+const PORT = process.env.PORT || 3000;
 
 const pdfPath = path.join(__dirname, 'public', 'check', 'reference-1242080012023055.pdf');
 
@@ -13,20 +12,13 @@ app.get('/', (req, res) => {
 app.get('/check/reference-1242080012023055.pdf', (req, res) => {
   res.download(pdfPath, 'reference-1242080012023055.pdf', (err) => {
     if (err) {
-      res.status(500).json({
-        result: -1,
-        message: 'Ошибка при скачивании файла'
-      });
+      res.status(500).json({ result: -1, message: 'Ошибка при скачивании файла' });
     }
   });
 });
 
 app.use((req, res) => {
-  res.status(403).json({
-    result: -1,
-    message: 'Access denied'
-  });
+  res.status(404).json({ result: -1, message: 'Выписка не найдена' });
 });
 
-
-
+app.listen(PORT, () => console.log(`Сервер запущен на порту ${PORT}`));
